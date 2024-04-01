c87cef269732cb9b1c9df438b4d7ea13b0c076ec
Harley <77483822+BigMark824@users.noreply.github.com>
2024-04-02 01:56:38 +1100

    add instructions

--- Include/assemblyHandler.h
+++ Include/assemblyHandler.h
@@ -2,20 +2,58 @@
 #define ASSEMBLY_HANDLER_H
 
 #include <memoryHandler.h>
+#include <cpuHandler.h>
 #include <stdio.h>
 #include <stdint.h>
 
 enum instruction
 {
     LDA,
-    STA
+    STA,
+    LDX,
+    STX,
+    ADC,
+    NOP,
+    STY,
+    LDY
 };
 
-struct instruction_set {
+struct instruction_set
+{
     enum instruction type;
     uint8_t value;
-};  
+};
 
-void CPU(struct instruction_set instSet);
+void CPU(struct instruction_set instSet)
+{
+    struct registers reg;
+    switch (instSet.type)
+    {
+    case LDA:
+        reg.A = instSet.value;
+        break;
+    case STA:
+        mem[instSet.value] = reg.A;
+        break;
+    case LDX:
+        reg.X = instSet.value;
+        break;
+    case STX:
+        mem[instSet.value] = reg.X;
+        break;
+    case ADC:
+        reg.A =+ instSet.value;
+        break;
+    case LDY:
+        reg.Y = instSet.value;
+        break;
+    case STY:
+        mem[instSet.value] = reg.Y;
+        break;
+    case NOP:
+        break;
+    }
+
+}
 
 #endif
--- Source/main.c
+++ Source/main.c
@@ -6,11 +6,12 @@
 #include <stdlib.h>
 typedef uint8_t byte;
 
-int main() {
+int main()
+{
 
     struct registers reg;
     struct flags flag;
-    // on startup initialise everything to 0
+
     reg.A = 0;
     reg.X = 0;
     reg.Y = 0;
@@ -21,27 +22,30 @@ int main() {
     flag.I = 0;
     flag.N = 0;
     flag.V = 0;
-    for (int i = 0; i < MEM; i++) {
+    for (int i = 0; i < MEM; i++)
+    {
         mem[i] = i % 256;
     }
     mem[0x8000] = 0x0000;
-    printf("A register: %u\n", reg.A);
-    printf("X register: %u\n", reg.X);
-    printf("Y register: %u\n", reg.Y);
 
-    mem[0x0001] = reg.A;
-    printf("Program Counter: 0x%x\n", mem[0x1000] + reg.PC);
-    printf("Values in memory:\n");
-    printf("Values in memory from address 0x8000:\n");
-    for (int i = 0; i < MEM; i++) {
+    for (int i = 0; i < MEM; i++)
+    {
         mem[i] = 0;
     }
-
     FILE *assemblyFile = fopen("test.asm", "r");
-    if (assemblyFile == NULL) {
+    if (assemblyFile == NULL)
+    {
         perror("Error opening file");
         return EXIT_FAILURE;
     }
 
-    return 0; 
+    char line[100];
+    printf("\nContents of Assembly:\n");
+    while (fgets(line, sizeof(line), assemblyFile) != NULL)
+    {
+        printf("%s", line);
+    }
+    printf("\n");
+    fclose(assemblyFile);
+    return 0;
 }
